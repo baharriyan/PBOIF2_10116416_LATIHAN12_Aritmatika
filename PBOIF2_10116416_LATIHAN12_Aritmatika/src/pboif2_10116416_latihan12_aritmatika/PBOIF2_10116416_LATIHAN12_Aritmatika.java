6e1468645a9d6bf5e1ac2277ebf88c523dce87d8
baharriyan <indonesiariyan@gmail.com>
2020-10-20 19:03:51 +0700

    first commit

---
+++ PBOIF2_10116416_LATIHAN12_Aritmatika/src/pboif2_10116416_latihan12_aritmatika/PBOIF2_10116416_LATIHAN12_Aritmatika.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116416_latihan12_aritmatika;
+
+/**
+ *
+ * @author Riyan Bahar
+ * Nama  : Riyan Bahar
+ * Kelas : IF-2
+ * Nim   : 10116416
+ * 
+ */
+public class PBOIF2_10116416_LATIHAN12_Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("b / a = " + (b/a));
+        System.out.println("b % a = " + (b%a));
+        System.out.println("c % a = " + (c%a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+    
+}
